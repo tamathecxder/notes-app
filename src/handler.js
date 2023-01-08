91106d83b07d9fa5e37b20fcc8c94766d27da2bd
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-01-08 13:07:25 +0700

    install nanoid dan setup routes, notes dan handler file

---
+++ src/handler.js
@@ -0,0 +1,5 @@
+function addNoteHandler(request, h) {
+  const { title, tags, body } = request.payload;
+}
+
+module.exports = { addNoteHandler };
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: () => 'Ini adalah notes page',
+  },
+];
+
+module.exports = routes;
