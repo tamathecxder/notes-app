999590010442fc101a5c32fc756b877ebff87725
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-01-08 14:53:38 +0700

    Membuat dan menjalankan fungsi untuk edit note by ID

--- src/handler.js
+++ src/handler.js
@@ -68,4 +68,41 @@ const getNoteByIdHandler = (request, h) => {
   return response;
 };
 
-module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler };
+const editNoteByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const { title, tags, body } = request.payload;
+  const updatedAt = new Date().toISOString();
+
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index !== -1) {
+    notes[index] = {
+      ...notes[index],
+      title,
+      tags,
+      body,
+      updatedAt,
+    };
+
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil diperbarui',
+    });
+
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Gagal memperbarui catatan, ID tidak ditemukan!',
+  });
+
+  response.code(500);
+  return response;
+};
+
+module.exports = {
+  addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler,
+};
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,7 @@
-const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require('./handler');
+const {
+  addNoteHandler, getAllNotesHandler, getNoteByIdHandler,
+  editNoteByIdHandler,
+} = require('./handler');
 
 const routes = [
   {
@@ -15,6 +18,11 @@ const routes = [
     method: 'GET',
     path: '/notes/{id}',
     handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
   },
 ];
 
