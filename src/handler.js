e66eef6746d919fbe13607e1e8abcdb376c9877d
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-01-08 13:18:56 +0700

    Membuat fungsi untuk menambahkan note baru

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,41 @@
+const { nanoid } = require('nanoid');
+const notes = require('./notes');
+
 function addNoteHandler(request, h) {
   const { title, tags, body } = request.payload;
+
+  const id = nanoid(16);
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
+
+  const newNote = {
+    id, title, tags, body, createdAt, updatedAt,
+  };
+
+  notes.push(newNote);
+
+  const isSuccess = notes.filter((note) => note.id === id).length > 0;
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil ditambahkan',
+      data: {
+        noteId: id,
+      },
+    });
+
+    response.code(201);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Oops! Catatan gagal ditambahkan',
+  });
+
+  response.code(500);
+  return response;
 }
 
 module.exports = { addNoteHandler };
