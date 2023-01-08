dad42583e9f5fd00c2ef0f655a6387c91d5b27f7
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-01-08 12:57:23 +0700

    Init server dan config ulang eslint

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    host: 'localhost',
+    port: 5000,
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
