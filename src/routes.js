3024ceca5557ce7cacc89c8eac6447594bf34cba
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-01-08 13:28:52 +0700

    Mengatasi CORS dengan menerapkan all origin pada host

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
+const { addNoteHandler } = require('./handler');
+
 const routes = [
   {
     method: 'POST',
     path: '/notes',
-    handler: () => 'Ini adalah notes page',
+    handler: addNoteHandler,
   },
 ];
 
--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
     host: 'localhost',
     port: 5000,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   });
+
+  server.route(routes);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
